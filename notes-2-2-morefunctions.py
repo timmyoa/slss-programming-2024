1ee414003844152255ef56a2b0e9234a2eb8344b
Tim Zou <tz000125@sd38.bc.ca>
2024-04-05 13:48:23 -0700

    add

--- notes-2-2-morefunctions.py
+++ notes-2-2-morefunctions.py
@@ -4,6 +4,21 @@
 
 # U= untracked || 
 # save process: save-> commit-> push 
-#(save, commit is in here. push is in github)
+# (save, commit is in here. push is in github)
 
+#Multiply strings
+greeting="hello"
+print(greeting*2)
 
+print("The quick brown dog fox jumps over the lazy dog."*2)
+
+def star(num):
+    value=""
+    #returns given number of *
+    if num==0
+        value=="nothing"
+    elif num>=1:
+        value="*"*num
+    return value
+
+print(star(100))
