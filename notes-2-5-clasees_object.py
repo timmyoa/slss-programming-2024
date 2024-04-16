5df43ee20d5617a523f720119d66e3543e037709
Tim Zou <tz000125@sd38.bc.ca>
2024-04-16 09:17:02 -0700

    add

---
+++ notes-2-5-clasees_object.py
@@ -0,0 +1,48 @@
+# classes and objects 
+
+#big ideas :
+# -classes allow us to couple data and functions together
+# -object are the actual representives of the classes in our python scripits
+
+#create a pokemon class; this repersnet a pokenon
+
+class Pokemon: #use capital letter for class name(conventional)
+    def __init__(self): 
+        """ special method/ function called constuctor. contains all the properties/variable that describe a pokemon"""
+        self.name = ""
+        self.id= 0
+        self.weight=0
+        self.height=0
+        self.type="normal"
+        self.actual_cry="114514!!" #the defulte value
+
+        print("a new pokemon is created")
+    def cry(self):
+        """this is the sound: pikapi
+        returns the string of the sound"""
+        return self.actual_cry
+    def eat(self,food:str):
+        "where to feed the pokemon, what it eats"
+        if food.lower()=="berry":
+            return f"{self.name} ate the berry"
+        if food.lower()=="potion":
+            return " it healed!"
+        else:
+            return f"smacks the {food} away"
+
+#create two new pokemon using our class
+# make one pokemon that is pikachu
+# make one pokemon of choice
+pokemon_one = Pokemon()
+
+#chaning properties
+print(pokemon_one.name)#gives nothing
+pokemon_one.name="pikachu"
+print(pokemon_one.name)
+pokemon_one.id="123213123"
+pokemon_one.type="eletric"
+
+#using cry method
+pokemon_one.actual_cry="11451514!!!!!!!!!!!!!"
+print(pokemon_one.actual_cry())
+print(pokemon_one.eat("berry"))
