260f659d31e15e935248b8e3b45eb33dc43e1674
Tim Zou <tz000125@sd38.bc.ca>
2024-04-29 08:32:56 -0700

    add

--- notes-3-1-pygame-boilderplate-sprites.py
+++ notes-3-1-pygame-boilderplate-sprites.py
@@ -12,7 +12,7 @@
 # -- rect(rectange) -> hitbox representation
 
 import pygame
-
+import random
 class Dvdlogo(pygame.sprite.Sprite):
     #represent dvd logo sprite
     def __init__(self):
@@ -21,8 +21,27 @@ class Dvdlogo(pygame.sprite.Sprite):
         self.image = pygame.image.load("./Images/dvd-logo.png")
 
         self.rect= self.image.get_rect()
+        # new properity: velocity of the dvd logo
+        self.vel_x=random.randrange(3,35)
+        self.vel_y=random.randrange(3,35)
+        self.rect.centerx = random.randrange(100,1100)
+        self.rect.centery = random.randrange(100,620)
 
-#boilerplate:
+    def update(self):
+        self.rect.x+=self.vel_x
+        self.rect.y+=self.vel_y
+        #bounce if reach bottom
+        if self.rect.bottom > 720:
+            self.vel_y *= -1
+        elif self.rect.top <0:
+            self.vel_y *=-1
+        if self.rect.right>1280:
+            self.vel_x*=-1
+        elif self.rect.left<0:
+            self.vel_x*=-1
+
+
+#boilerplate: below
 
 def start():
     """Environment Setup and Game Loop"""
@@ -51,11 +70,15 @@ def start():
     pygame.display.set_caption("SUPER DUPER UBER LUBER CHIPPI CHAPPA CHOPPY LABA OMEGA FUN DVD LOGO SCREENSAVER BOUNCER CORNER FUN FOR THE FAMILY AND HAPPY AND COOL COOKIE FOREVER")
     
     #create a dvd logo object
+
     dvdlogo=Dvdlogo()
+
     #create group of spirite
     all_sprites=pygame.sprite.Group()
     #add dvd logo to group of sprites
-    all_sprites.add(dvdlogo)
+    for i in range(1):
+        all_sprites.add(Dvdlogo())
+
 
 
     # --MAIN LOOP--
@@ -64,8 +87,15 @@ def start():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+            if event.type==pygame.KEYDOWN:
+                if pygame.key.get_pressed()[pygame.K_SPACE]:
+                    all_sprites.add(Dvdlogo())
 
         # --- Update the world state
+        #update the location of the dvdlogo
+        #    dvdlogo.rect.x +dedlogo.vel_x
+        #    dvdlogo.rect.y+dvdlogo.vel_y
+        all_sprites.update()
 
         # --- Draw items
         screen.fill(BLACK)
